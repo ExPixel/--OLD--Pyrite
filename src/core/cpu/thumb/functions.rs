7b7ff67ab84f378335b7111af1a89b333e85ec05
Adolph C <adolph.celestin@gmail.com>
2016-01-03 23:15:29 -0500

    THUMB DataProc instructions

--- src/core/cpu/thumb/functions.rs
+++ src/core/cpu/thumb/functions.rs
@@ -1,5 +1,15 @@
 use super::super::ArmCpu;
 
+pub fn thumb_fn_neg(cpu: &mut ArmCpu, _: u32, rhs: u32) -> u32 {
+	super::super::arm::functions::arm_fn_sub_s(cpu, 0, rhs)
+}
+
+pub fn thumb_fn_mul(cpu: &mut ArmCpu, lhs: u32, rhs: u32) -> u32 {
+	let result = lhs * rhs;
+	super::super::alu::set_nz_flags(cpu, result);
+	return result;
+}
+
 pub fn thumb_fn_ldr(cpu: &mut ArmCpu, address: u32, rd: u32) {
 
 }
--- src/core/cpu/thumb/thumb_dp_impl.rs
+++ src/core/cpu/thumb/thumb_dp_impl.rs
@@ -1,99 +1,106 @@
 use super::super::ArmCpu;
 use super::super::super::memory::GbaMemory;
+use super::super::arm::functions::*;
+use super::functions::thumb_fn_neg;
+use super::functions::thumb_fn_mul;
+use super::super::alu::*;
+
+/// Generates a THUMB ALU operation.
+macro_rules! gen_alu {
+	(
+		$instr_name:ident,
+		$operation:ident
+	) => (
+    	pub fn $instr_name(cpu: &mut ArmCpu, instr: u32) {
+			let rd = instr & 0x7;
+			let rs = (instr >> 3) & 0x7;
+			let _rd = cpu.rget(rd);
+			let _rs = cpu.rget(rs);
+			let result = $operation(cpu, rd, rs);
+			cpu.rset(rd, result);
+		}
+	)
+}
+
+/// Generates a THUMB ALU operation that does not write back.
+macro_rules! gen_alu_nw {
+	(
+		$instr_name:ident,
+		$operation:ident
+	) => (
+    	pub fn $instr_name(cpu: &mut ArmCpu, instr: u32) {
+			let rd = instr & 0x7;
+			let rs = (instr >> 3) & 0x7;
+			let _rd = cpu.rget(rd);
+			let _rs = cpu.rget(rs);
+			$operation(cpu, rd, rs)
+			// ^ not including the semi-colon causes an error to occur
+			// if I use a function that returns anything.
+		}
+	)
+}
 
 /// AND 
 /// Logical And
-pub fn thumb_dp_and(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_and, arm_fn_and_s);
 
 /// EOR 
 /// Logical Exclusive-or
-pub fn thumb_dp_eor(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_eor, arm_fn_eor_s);
 
 /// LSL 
 /// Logical Left-shift
-pub fn thumb_dp_lsl(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_lsl, arm_alu_llr_s);
 
 /// LSR 
 /// Logical Right-shift
-pub fn thumb_dp_lsr(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_lsr, arm_alu_lrr_s);
 
 /// ASR 
 /// Arithmetic Right-shift
-pub fn thumb_dp_asr(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_asr, arm_alu_arr_s);
 
 /// ADD 
 /// Add
-pub fn thumb_dp_add(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_add, arm_fn_add_s);
 
 /// SUB 
 /// Subtract
-pub fn thumb_dp_sub(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_sub, arm_fn_sub_s);
 
 /// ROR 
 /// Rotate right
-pub fn thumb_dp_ror(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_ror, arm_alu_rrr_s);
 
 /// TST 
 /// Test Bits (Logical And)
-pub fn thumb_dp_tst(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu_nw!(thumb_dp_tst, arm_fn_tst_s);
 
 /// NEG 
 /// Negate (Subtract from zero)
-pub fn thumb_dp_neg(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_neg, thumb_fn_neg);
 
 /// CMP 
 /// Compare (Subtract)
-pub fn thumb_dp_cmp(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu_nw!(thumb_dp_cmp, arm_fn_cmp_s);
 
 /// CMN 
 /// Compare negative (Add)
-pub fn thumb_dp_cmn(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu_nw!(thumb_dp_cmn, arm_fn_cmn_s);
 
 /// ORR 
 /// Logical Or
-pub fn thumb_dp_orr(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_orr, arm_fn_orr_s);
 
 /// MUL 
 /// Multiply
-pub fn thumb_dp_mul(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_mul, thumb_fn_mul);
 
 /// BIC 
 /// Bit Clear (NAND)
-pub fn thumb_dp_bic(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_bic, arm_fn_bic_s);
 
 /// MVN 
 /// Move negative (NOT)
-pub fn thumb_dp_mvn(cpu: &mut ArmCpu, instr: u32) {
-	// #TODO
-}
+gen_alu!(thumb_dp_mvn, arm_fn_mvn_s);
 
