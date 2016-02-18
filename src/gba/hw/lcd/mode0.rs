9ef5cd1b385d19cd138e292dba92f85b7ec689d8
Adolph C <adolph.celestin@gmail.com>
2016-02-18 13:27:04 -0500

    Some text mode stuff.
    Not sure why I find text mode so difficult to wrap my head around, but for some reason it's taking me a while.

--- src/gba/hw/lcd/mode0.rs
+++ src/gba/hw/lcd/mode0.rs
@@ -5,4 +5,20 @@ use super::tiles::*;
 const TILE_ADDR: (u32, u32) = (0x06000000, 0x0600FFFF);
 
 pub fn render_mode_0(dispcnt: u16, memory: &GbaMemory, line: u16, lines: &mut GbaDisplayLines) {
+	let mut try_render_bg = |enable: u16, bgcnt: ioreg::IORegister16, bghofs: ioreg::IORegister16, bgvofs: ioreg::IORegister16| {
+		if ((dispcnt >> enable) & 1) != 0 {
+			draw_tiles_text_mode(
+				memory.get_reg(bgcnt), 
+				memory.get_reg(bghofs),
+				memory.get_reg(bgvofs),
+				memory,
+				line,
+				&mut lines.bg0);
+		}
+	};
+
+	try_render_bg(0x8, ioreg::BG0CNT, ioreg::BG0HOFS, ioreg::BG0VOFS);
+	try_render_bg(0x9, ioreg::BG1CNT, ioreg::BG1HOFS, ioreg::BG1VOFS);
+	try_render_bg(0xA, ioreg::BG2CNT, ioreg::BG2HOFS, ioreg::BG2VOFS);
+	try_render_bg(0xB, ioreg::BG3CNT, ioreg::BG3HOFS, ioreg::BG3VOFS);
 }
